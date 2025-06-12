de6db41ad53c7518dd42a1da5381ce3b179adb66
yyyrk <andrei.iurik@gmail.com>
2025-06-13 00:59:01 +0300

    chore: rubocop -A

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'test_helper'
+require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  if ENV['CAPYBARA_SERVER_PORT']
-    served_by host: 'rails-app', port: ENV['CAPYBARA_SERVER_PORT']
+  if ENV["CAPYBARA_SERVER_PORT"]
+    served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
 
     driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
       browser: :remote,

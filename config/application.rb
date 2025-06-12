0d95a9e8392123f675e98dd906de6de582912c7c
yyyrk <andrei.iurik@gmail.com>
2025-06-13 00:59:01 +0300

    chore: rubocop -A

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 Bundler.require(*Rails.groups)
 
@@ -16,13 +16,13 @@ module StackoverflowCloneDefinitiveEdition
     config.i18n.default_locale = :ru
     config.i18n.available_locales = [ :ru, :en ]
     config.i18n.fallbacks = [ :ru ]
-    config.time_zone = 'Moscow'
+    config.time_zone = "Moscow"
     config.active_record.default_timezone = :local
 
     # Явно указываем кодировку
-    config.encoding = 'utf-8'
+    config.encoding = "utf-8"
 
     # Загружаем локали из вложенных директорий
-    config.i18n.load_path += Rails.root.glob('config/locales/**/*.{rb,yml}')
+    config.i18n.load_path += Rails.root.glob("config/locales/**/*.{rb,yml}")
   end
 end

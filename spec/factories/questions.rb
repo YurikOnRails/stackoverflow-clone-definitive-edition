30ec132259af4f059da90534853995d441cc9d3f
yyyrk <andrei.iurik@gmail.com>
2025-06-14 01:33:42 +0300

    feat: add controllers and RSpec tests

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,7 +2,11 @@
 
 FactoryBot.define do
   factory :question do
-    title { "Важный вопрос о Ruby on Rails" }
-    body { "Это тело вопроса, которое должно быть не менее 10 символов для прохождения валидации" }
+    sequence(:title) { |n| "Важный вопрос о Ruby on Rails #{n}" }
+    sequence(:body)  { |n| "Это тело вопроса №#{n}, которое должно быть не менее 10 символов для прохождения валидации" }
+
+    trait :invalid do
+      title { nil }
+    end
   end
 end

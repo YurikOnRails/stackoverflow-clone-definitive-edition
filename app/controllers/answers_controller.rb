7787458a5090de04db69a1f3a5bfcd3e9526f029
yyyrk <andrei.iurik@gmail.com>
2025-06-14 01:45:25 +0300

    feat: add answers with tests

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class AnswersController < ApplicationController
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.build(answer_params)
+    if @answer.save
+      redirect_to question_path(@question)
+    else
+      render "questions/show"
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+end

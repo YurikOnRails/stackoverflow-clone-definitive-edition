efee27584d0ce8dbb4dfc8529ab8d18654d269fd
yyyrk <andrei.iurik@gmail.com>
2025-06-23 07:03:30 +0300

    feat: add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,49 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Cleaning database...'
+Answer.destroy_all
+Question.destroy_all
+User.destroy_all
+
+puts 'Creating users...'
+
+andrey = User.create!(
+  email: 'andrey@gmail.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+random_users = 4.times.map do
+  User.create!(
+    email: FFaker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+
+users = [andrey] + random_users
+puts 'Users created.'
+
+puts 'Creating questions...'
+questions = users.flat_map do |user|
+  2.times.map do
+    user.questions.create!(
+            title: FFaker::Lorem.sentence(5).delete_suffix('.'),
+      body: FFaker::Lorem.paragraph
+    )
+  end
+end
+puts 'Questions created.'
+
+puts 'Creating answers...'
+questions.each do |question|
+  3.times do
+    question.answers.create!(
+      body: FFaker::Lorem.paragraph,
+      user: users.sample
+    )
+  end
+end
+puts 'Answers created.'
+
+puts 'Finished seeding!'
